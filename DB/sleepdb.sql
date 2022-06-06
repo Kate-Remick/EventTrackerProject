0e3e61770dedd000a7ceee716c1484d83cf89fe7
Katherine <remick.ka@gmail.com>
2022-06-05 17:37:45 -0700

    readme and sql database file

--- DB/sleepdb.sql
+++ DB/sleepdb.sql
@@ -15,21 +15,59 @@ CREATE SCHEMA IF NOT EXISTS `sleepdb` DEFAULT CHARACTER SET utf8 ;
 USE `sleepdb` ;
 
 -- -----------------------------------------------------
+-- Table `sleepdb`.`evening_activity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sleepdb`.`evening_activity` ;
+
+CREATE TABLE IF NOT EXISTS `sleepdb`.`evening_activity` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sleepdb`.`workout`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sleepdb`.`workout` ;
+
+CREATE TABLE IF NOT EXISTS `sleepdb`.`workout` (
+  `id` INT NOT NULL,
+  `time_of_day` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `sleepdb`.`sleep_period`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `sleepdb`.`sleep_period` ;
 
 CREATE TABLE IF NOT EXISTS `sleepdb`.`sleep_period` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `start` DATETIME NULL,
   `end` VARCHAR(45) NULL,
   `duration` DECIMAL(2) NULL,
-  `had_alchohol` TINYINT NOT NULL,
+  `had_alcohol` TINYINT NOT NULL,
   `large_dinner` TINYINT NOT NULL,
   `excercised` TINYINT NOT NULL,
-  `evening_activity` VARCHAR(45) NULL,
   `took_nap` TINYINT NULL,
-  PRIMARY KEY (`id`))
+  `quality` INT NULL,
+  `evening_activity_id` INT NOT NULL,
+  `workout_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_sleep_period_evening_activity1_idx` (`evening_activity_id` ASC),
+  INDEX `fk_sleep_period_workout1_idx` (`workout_id` ASC),
+  CONSTRAINT `fk_sleep_period_evening_activity1`
+    FOREIGN KEY (`evening_activity_id`)
+    REFERENCES `sleepdb`.`evening_activity` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_sleep_period_workout1`
+    FOREIGN KEY (`workout_id`)
+    REFERENCES `sleepdb`.`workout` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -44,11 +82,70 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `sleepdb`.`evening_activity`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `sleepdb`;
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (1, 'Read a Book');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (2, 'Painting');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (3, 'Video Games');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (4, 'Coding');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (5, 'Work');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (6, 'Cleaning');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (7, 'Yard Work');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (8, 'Watched TV Show');
+INSERT INTO `sleepdb`.`evening_activity` (`id`, `name`) VALUES (9, 'Watched Movie');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `sleepdb`.`workout`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `sleepdb`;
+INSERT INTO `sleepdb`.`workout` (`id`, `time_of_day`) VALUES (1, 'Morning');
+INSERT INTO `sleepdb`.`workout` (`id`, `time_of_day`) VALUES (2, 'Noon');
+INSERT INTO `sleepdb`.`workout` (`id`, `time_of_day`) VALUES (3, 'Afternoon');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `sleepdb`.`sleep_period`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `sleepdb`;
-INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alchohol`, `large_dinner`, `excercised`, `evening_activity`, `took_nap`) VALUES (1, NULL, NULL, 8.25, 0, 0, 0, 'Read Book', 0);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (1, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 7, 1, 1);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (2, '2022-05-28 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 7, 2, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (3, '2022-05-27 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 8, 2, 3);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (4, '2022-05-26 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 8, 2, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (5, '2022-05-25 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 8, 3, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (6, '2022-05-24 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 1, 1, 0, 7, 3, 3);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (7, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 0, 1, 0, 7, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (8, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 0, 1, 0, 6, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (9, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 0, 1, 0, 4, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (10, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 4, 6, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (11, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 4, 6, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (12, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 5, 7, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (13, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 0, 0, 2, 7, 3);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (14, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 1, 0, 0, 3, 1, 1);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (15, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 0, 1, 2, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (16, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 4, 3, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (17, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 5, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (18, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 5, 6, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (19, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 5, 7, 3);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (20, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 1, 0, 7, 3, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (21, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 1, 0, 0, 7, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (22, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 0, 0, 5, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (23, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 1, 0, 0, 0, 4, 1, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (24, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 3, 2, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (25, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 5, 2, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (26, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 6, 1, 1);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (27, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 7, 3, 2);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (28, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 0, 0, 5, 4, NULL);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (29, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 2, 2, 1);
+INSERT INTO `sleepdb`.`sleep_period` (`id`, `start`, `end`, `duration`, `had_alcohol`, `large_dinner`, `excercised`, `took_nap`, `quality`, `evening_activity_id`, `workout_id`) VALUES (30, '2022-05-29 22:05:00', '2022-05-30 06:10:00', 8.10, 0, 0, 1, 0, 7, 2, 2);
 
 COMMIT;
 
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# EventTrackerProject
+# Event Tracker Project
+
+## Overview
+This project is the backend for an event tracker (Skill Distillery Week 12 Homework). It includes a MySQL database and utilizes Spring Data JPA and Spring Web features to make the backend fully accessible to a future front end. All entities were tested using Junit and all routes below have been tested using postman.
+
+## Description and Use (includes expected routes)
+Sleep can be hard sometimes, and getting a good night's rest is elusive to many people. One thing that can assist in achieving the dream(s) is an understanding of what things work best for you developed from data about your sleep history. This program takes data from a user about each night's sleep, records it, and can give back recommendations based on how the user felt the quality was for each night. The database in this case was pre-populated with 30 days of sleep.
+
+Expected Routes:
+* List all nights of sleep: (Get) http://localhost:8083/api/sleep
+* Show one night's sleep: (Get) http://localhost:8083/api/sleep/{id}
+* Add a night of sleep: (Post) http://localhost:8083/api/sleep
+  JSON for adding a sleep object:
+  {
+    "start": "2022-05-30T21:05:00",
+    "end": "2022-05-31T05:10:00",
+    "hadAlcohol": false,
+    "largeDinner": false,
+    "excercised": true,
+    "tookNap": false,
+    "quality": 7,
+    "eveningActivity": {
+        "id": 1,
+        "name": "Read a Book"
+    },
+    "workout": {
+        "id": 1,
+        "timeOfDay": "Morning"
+    }
+}
+* Edit a night of sleep: (Put) http://localhost:8083/api/sleep/{id}
+* Delete a night of sleep: (Delete) http://localhost:8083/api/sleep/{id}
+* List nights of sleep by time of day worked out: (Get) http://localhost:8083/api/sleep/workout/{id}
+* List nights of sleep by evening activity: (Get) http://localhost:8083/api/sleep/activity/{id}
+* List nights of sleep by quality group (between first and second number): (Get) http://localhost:8083/api/sleep/4/8
+* List recommended evening activities: (Get) http://localhost:8083/api/recommendations/goodActivities
+* List worst evening activities for a good nights sleep: (Get) http://localhost:8083/api/recommendations/badActivities
+* Show recommendation for a large or small dinner: (Get) http://localhost:8083/api/recommendations/dinner
+* Show recommendation for weather user should avoid naps: (Get) http://localhost:8083/api/recommendations/nap
+* Show recommendation for when a user should work out (morning, noon, night): (Get) http://localhost:8083/api/recommendations/workout
+* Show recommendation for when a user should go to bed: (Get) http://localhost:8083/api/recommendations/sleepStart
+* Show recommendation for when a user should get up: (Get) http://localhost:8083/api/recommendations/sleepEnd
+* Show recommendation for sleep duration: (Get) http://localhost:8083/api/recommendations/duration
+* List all evening activities: (Get) http://localhost:8083/api/activities
+* Add/Create a new activity: (Post) http://localhost:8083/api/activities
+  JSON for adding an activity:
+  {
+        "name": "Social Event"
+    }
+* List all workouts (workout times): (Get) http://localhost:8083/api/workouts
+
+## Technologies used:
+* Java
+* MySQL Workbench
+* MySQL
+* Gradle
+* Git/GitHub
+* Java Persistance API (JPA)
+* RESTful services
+* Postman
+* Junit
+
+## Lessons Learned
+ * Getting elapsed time between two LocalDateTimes in Java can be difficult, as simple subtraction was not available. To get around this, I converted the hours to integers, and the minute to a decimals reflecting the percentage of the hour passed and subtracted the numbers after the conversion.
+ * Always, always reboot the server before testing changes that have been made.
